88ac93de0564e9f6b5cbc35de21caaa7073dcace
Luka Danelia <lukadanelia056@gmail.com>
2024-03-05 10:51:37 +0400

    Add project files.

---
+++ Controllers/Test.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace net.Controllers
+{
+    [Route("api/[Controller]")]
+    public class TestController : ControllerBase
+    {
+        public TestController() { }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new {message="BONJOUR"});
+        }
+    }
+}
